9529ffb846576c7c596315f7274fa92104f83815
Damien <afienkod@mail.gvsu.edu>
2023-06-09 19:47:12 -0400

    init

--- client/src/login/index.js
+++ client/src/login/index.js
@@ -1,44 +1,37 @@
 import LoginProtectedRoute from "../LoginProtectedRoute";
 import { Form } from 'react-bootstrap'
 
+import './login.css';
+
 const Login = () => {
 	return (
 		<LoginProtectedRoute shouldBeLoggedIn={false}>
-			<div>
-				<h2>Sign In</h2>
-				<Form autoComplete="false">
-					<div>
-						<label for="username">Username: </label>
-						<input type="text" name="username"></input>
-					</div>
-					<div>
-						<label for="password">Password: </label>
-						<input type="password" name="password"></input>
-					</div>
-					<input type="submit" value="Sign In"></input>
-				</Form>
-				<div>
-					<button>Login with Google</button>
-					<button>Login with Github</button>
+			<div id="login">
+				<div class="loginOption">
+					<h2>Sign In</h2>
+					<Form autoComplete="false">
+						<input type="text" placeholder="Username" name="username"></input>
+						<input type="password" placeholder="Password" name="password"></input>
+						<input class="submitButton" type="submit" value="Sign In"></input>
+					</Form>
+					<button class="oauthButton">
+						<img class="oauthIcon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+						<p>Sign in with Google</p>
+					</button>
+					<button class="oauthButton">
+						<img class="oauthIcon" src="./github-mark.svg"/>
+						<p>Sign in with GitHub</p>
+					</button>
 				</div>
-			</div>
-			<div>
-				<h2>Create Account</h2>
-				<Form autoComplete="false">
-					<div>
-						<label for="username">Username: </label>
-						<input type="text" name="username"></input>
-					</div>
-					<div>
-						<label for="password">Password: </label>
-						<input type="password" name="password"></input>
-					</div>
-					<div>
-						<label for="confirm-password">Confirm Password: </label>
-						<input type="password" name="confirm-password"></input>
-					</div>
-					<input type="submit" value="Sign Up"></input>
-				</Form>
+				<div class="loginOption">
+					<h2>Create Account</h2>
+					<Form autoComplete="false">
+						<input type="text" placeholder="Username" name="username"></input>
+						<input type="password" placeholder="Password" name="password"></input>
+						<input type="password" placeholder="Confirm Password" name="confirm-password"></input>
+						<input class="submitButton" type="submit" value="Sign Up"></input>
+					</Form>
+				</div>
 			</div>
 		</LoginProtectedRoute>
 	);
